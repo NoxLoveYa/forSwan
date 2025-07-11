5b720bccfbb156785cb6eda380d9c9ce7a1b214a
NoxLoveYa <ewan.rondeau@gmail.com>
2025-07-11 13:50:12 +0200

    better npc logic

--- src/ModulR/Services/NpcService.lua
+++ src/ModulR/Services/NpcService.lua
@@ -18,6 +18,15 @@ local currentDialogue = nil
 local connections = {}
 
 -- Local functions
+local function yap(text, textLabel)
+	task.spawn(function()
+		textLabel.Text = ""
+		for i = 1, #text do
+			textLabel.Text = textLabel.Text .. text:sub(i, i)
+			task.wait(0.02) -- Adjust the speed of the typing effect
+		end
+	end)
+end
 
 -- Init (qui va être supprimé lors de l'init du core)
 function NpcService:Initialize()
@@ -43,64 +52,20 @@ function NpcService:Initialize()
 			end
 
 			local choiceIndex = 1
-			for choiceName, _ in pairs(currentDialogue) do
-				local choiceButton: TextButton = ChoiceGui:FindFirstChild("Button" .. choiceIndex)
+			for choiceText, choiceData in pairs(currentDialogue) do
+				local choiceButton = ChoiceGui:FindFirstChild("Button" .. choiceIndex)
 				if not choiceButton then
-					warn(
-						"[NpcService] - Choice button not found for NPC:",
-						npc.Name,
-						"Choice:",
-						choiceName,
-						"Button" .. choiceIndex
-					)
-					continue
+					warn("[NpcService] - Choice button not found for choice index:", choiceIndex)
+					return
 				end
+				choiceButton.Text = choiceText
+				choiceButton.Visible = true
 
-				choiceButton.Text = choiceName -- Set the text of the choice button
 				table.insert(connections, choiceButton.MouseButton1Click:Connect(function()
-					currentDialogue = DialogueService:FollowDialogue(currentDialogue, choiceName)
-					task.spawn(function()
-						local currentText = ""
-						local endText = currentDialogue.Text
-						while currentText ~= endText do
-							currentText = endText:sub(1, #currentText + 1)
-							YapGui.TextLabel.Text = currentText
-							task.wait(0.015) -- Adjust the speed of text display
-						end
-					end)
-                    local choiceSave = {}
-                    for _, choiceName in pairs(currentDialogue.Choices) do
-                        choiceSave[choiceName] = DialogueService:GetSubDialogue(npc.Name, choiceName)
-                    end
-                    currentDialogue = choiceSave
-                    local subDialogueIndex = 1
-                    print("Current Dialogue:", currentDialogue)
-                    for subChoiceName, _ in pairs(currentDialogue) do
-                        local subChoiceButton: TextButton = ChoiceGui:FindFirstChild("Button" .. subDialogueIndex)
-                        if not subChoiceButton then
-                            warn(
-                                "[NpcService] - Sub choice button not found for NPC:",
-                                npc.Name,
-                                "Sub Choice:",
-                                subChoiceName,
-                                "Button" .. subDialogueIndex
-                            )
-                            continue
-                        end
-
-                        subChoiceButton.Text = subChoiceName -- Set the text of the sub choice button
-                        subChoiceButton.Visible = true -- Make the sub choice button visible
-                        subDialogueIndex += 1
-                    end
-                    for i = subDialogueIndex, 3 do
-                        local subChoiceButton: TextButton = ChoiceGui:FindFirstChild("Button" .. i)
-                        if subChoiceButton then
-                            subChoiceButton.Visible = false -- Hide unused buttons
-                        end
-                    end
+					yap(choiceData.Text, YapGui.TextLabel)
 				end))
+
 				choiceIndex += 1
-				choiceButton.Visible = true -- Make the choice button visible
 			end
 
 			ChoiceGui.Parent = Players.LocalPlayer.PlayerGui
